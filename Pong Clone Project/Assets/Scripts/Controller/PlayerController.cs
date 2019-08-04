76fa1f18666baeb3d4ef628d6a8a00385168f7d8
Matthew R <Mattsdev107@gmail.com>
2019-08-04 18:59:48 -0500

    Setting Up The Player Controller

--- Pong Clone Project/Assets/Scripts/Controller/PlayerController.cs
+++ Pong Clone Project/Assets/Scripts/Controller/PlayerController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public enum PlayerSelection { PlayerOne, PlayerTwo, AIPlayer };
+    public PlayerSelection playerSelected;
+
+    private Rigidbody2D rb;
+    private GameObject ball;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        ball = GameObject.FindGameObjectWithTag("ball");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if(playerSelected != PlayerSelection.AIPlayer) {
+            PlayerMovement();
+        }else if (playerSelected == PlayerSelection.AIPlayer) {
+            AIMovement();
+        }
     }
 }
