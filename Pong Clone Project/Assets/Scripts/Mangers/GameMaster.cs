8e9ee522f44a81c629356e0f4b9395cd4fca609d
Matthew R <Mattsdev107@gmail.com>
2019-08-07 18:25:10 -0500

    In GameMaster script I add spawn method

--- Pong Clone Project/Assets/Scripts/Mangers/GameMaster.cs
+++ Pong Clone Project/Assets/Scripts/Mangers/GameMaster.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour
-{
+public class GameMaster : MonoBehaviour{
+
+    GameObject ball;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Start(){
+        SpawnBall();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void SpawnBall() {
+        ball = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Ball", typeof(GameObject)));
+        ball.transform.position = Vector2.zero;
     }
 }
